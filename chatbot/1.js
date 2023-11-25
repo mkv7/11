0076f8bac744a0393de5b4fa799ff1e576f8a48d
mkv7 <mkv12moaeez@gmail.com>
2023-11-25 17:39:55 +0000

    rating features added

--- chatbot/1.js
+++ chatbot/1.js
@@ -1,40 +1,49 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const chatLog = document.getElementById('chat-log');
-    const userInput = document.getElementById('user-input');
-    const sendButton = document.getElementById('send-button');
-  
-    sendButton.addEventListener('click', function () {
-      const userMessage = userInput.value.trim();
-  
-      if (userMessage !== '') {
-        // Display user message in the chat log
-        displayMessage('You', userMessage);
-  
-        // Simulate the chatbot's response (you can replace this with your own logic)
-        const botResponse = getBotResponse(userMessage);
-  
-        // Display the chatbot's response
-        displayMessage('Chatbot', botResponse);
-  
-        // Clear the user input field
-        userInput.value = '';
-      }
-    });
-  
-    function displayMessage(sender, message) {
-      const messageElement = document.createElement('div');
-      messageElement.classList.add('message');
-      messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
-      chatLog.appendChild(messageElement);
-  
-      // Scroll to the bottom of the chat log
-      chatLog.scrollTop = chatLog.scrollHeight;
-    }
-  
-    function getBotResponse(userMessage) {
-      // Replace this with your own chatbot logic
-      // Here's a simple example that just echoes the user's message
-      return `You said: ${userMessage}`;
+  const chatLog = document.getElementById('chat-log');
+  const userInput = document.getElementById('user-input');
+  const sendButton = document.getElementById('send-button');
+  const rateButton = document.getElementById('rate-button');
+
+  sendButton.addEventListener('click', function () {
+    const userMessage = userInput.value.trim();
+
+    if (userMessage !== '') {
+      // Display user message in the chat log
+      displayMessage('You', userMessage);
+
+      // Simulate the chatbot's response
+      const botResponse = getBotResponse(userMessage);
+
+      // Display the chatbot's response
+      displayMessage('Chatbot', botResponse);
+
+      // Clear the user input field
+      userInput.value = '';
     }
   });
-  
+
+  rateButton.addEventListener('click', function () {
+    const rating = document.querySelector('input[name="rating"]:checked')?.value;
+    if (rating) {
+      displayMessage('You', `Rated: ${rating}/5`);
+      // Logic to handle the rating can be added here
+    } else {
+      alert("Please select a rating.");
+    }
+  });
+
+  function displayMessage(sender, message) {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+    messageElement.innerHTML = `<strong>${sender}:</strong> ${message}`;
+    chatLog.appendChild(messageElement);
+
+    // Scroll to the bottom of the chat log
+    chatLog.scrollTop = chatLog.scrollHeight;
+  }
+
+  function getBotResponse(userMessage) {
+    // Replace with your chatbot logic
+    return `You said: ${userMessage}`;
+  }
+});
